62097ac7588dd92390574c09afd00f82e28933c4
陈真 <chenzhen7@xiaoniu66.com>
2017-06-21 18:07:06 +0800

    修改uiframe框架包结构

--- CMXnUiFrame/src/main/java/com/xn/uiframe/ElementView.java
+++
@@ -1,9 +0,0 @@
-package com.xn.uiframe;
-
-/**
- * Created by xn068074 on 2017/6/15.
- */
-
-public enum ElementView {
-    HeaderView, TopView, CenterView,CenterMaskView, BottomView, DialogView, ErrorView, LoadView, ExtraView
-}
